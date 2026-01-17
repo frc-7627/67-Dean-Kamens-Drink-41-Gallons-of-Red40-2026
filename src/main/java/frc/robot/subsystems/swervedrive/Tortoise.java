c0a642efdd9b516669f54ee83f2737c6a624dfd2
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-16 16:13:36 -0800

    Linted it all!

--- src/main/java/frc/robot/subsystems/swervedrive/Tortoise.java
+++ src/main/java/frc/robot/subsystems/swervedrive/Tortoise.java
@@ -2,6 +2,6 @@ package frc.robot.subsystems.swervedrive;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-public class Tortoise extends SubsystemBase{
-    
+public class Tortoise extends SubsystemBase {
+
 }
