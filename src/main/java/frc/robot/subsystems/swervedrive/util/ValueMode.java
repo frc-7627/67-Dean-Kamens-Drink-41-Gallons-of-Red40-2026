11cb72dcb70ef61ae223aa1e4b6fd96f68fbdbae
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-21 09:41:45 -0800

    Docs in ValueMode

--- src/main/java/frc/robot/subsystems/swervedrive/util/ValueMode.java
+++ src/main/java/frc/robot/subsystems/swervedrive/util/ValueMode.java
@@ -3,6 +3,9 @@ package frc.robot.subsystems.swervedrive.util;
 public enum ValueMode {
     PUSH_ONLY, PUSH_AND_PULL_CONST, PUSH_AND_PULL;
 
+    /**
+     * @return Whether the mode allows pulling.
+     */
     public boolean isPull() {
         return switch (this) {
             case PUSH_ONLY -> false;
