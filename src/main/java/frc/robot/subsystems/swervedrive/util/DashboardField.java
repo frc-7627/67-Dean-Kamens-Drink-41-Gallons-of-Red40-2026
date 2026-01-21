34d8a5a068cd40fde204be1188935064ce3dba82
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-21 09:24:31 -0800

    More docs.

--- src/main/java/frc/robot/subsystems/swervedrive/util/DashboardField.java
+++ src/main/java/frc/robot/subsystems/swervedrive/util/DashboardField.java
@@ -11,17 +11,38 @@ public interface DashboardField<Inner> {
      */
     Inner getInner();
 
+    /**
+     * Set the inner value.
+     * 
+     * @param inner the inner value.
+     */
     void setInner(Inner inner);
 
+    /**
+     * Push the field to the dashboard.
+     */
     void push();
 
+    /**
+     * Pull the field from the dashboard.
+     */
     void pull();
 
+    /**
+     * Update the field.
+     * 
+     * Push and pull the field to and from the dashboard.
+     */
     default void update() {
         push();
         pull();
     }
 
+    /**
+     * Update all the provided fields.
+     * 
+     * @param fields the provided fields.
+     */
     public static void updateAll(List<DashboardField<?>> fields) {
         for (DashboardField<?> field : fields) {
             field.update();
