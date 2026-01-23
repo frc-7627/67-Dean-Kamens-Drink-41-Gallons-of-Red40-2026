382b698f70c1464bbc999e7987fc864625ae321b
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-23 07:42:23 -0800

    Fixed getDefaultValue in value dashboard field.

--- src/main/java/frc/robot/subsystems/util/dashboard/ValueDashboardField.java
+++ src/main/java/frc/robot/subsystems/util/dashboard/ValueDashboardField.java
@@ -1,16 +1,19 @@
 package frc.robot.subsystems.util.dashboard;
 
 public abstract class ValueDashboardField<Inner> extends BaseDashboardField<Inner> {
-    private final Inner initialValue;
+    private final Inner defaultValue;
 
     protected ValueDashboardField(String subsystemName, String fieldName, Inner initialValue, FieldMode fieldMode) {
         super(subsystemName, fieldName, initialValue, fieldMode);
 
-        this.initialValue = initialValue;
+        this.defaultValue = fieldMode.isPull() ? initialValue : null;
     }
 
     @Override
     protected final Inner getDefaultValue() {
-        return initialValue;
+        if (!getFieldMode().isPull()) {
+            throw new IllegalStateException("Default value only exists for pulling fields!");
+        }
+        return defaultValue;
     }
 }
