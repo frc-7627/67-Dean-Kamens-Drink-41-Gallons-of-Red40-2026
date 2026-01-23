e6ca5b98186da7f51ddfd5bdd17934bd91d7a1ca
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-23 15:59:25 -0800

    Added functionality to bind an action for when an alliance is set in the driver station.

--- src/main/java/frc/robot/subsystems/GameInfo.java
+++ src/main/java/frc/robot/subsystems/GameInfo.java
@@ -1,6 +1,8 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.event.BooleanEvent;
+import edu.wpi.first.wpilibj.event.EventLoop;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import java.util.Optional;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -15,13 +17,23 @@ public class GameInfo extends SubsystemBase {
         AUTO, TRANSITION, TELEOP_1, TELEOP_2, TELEOP_3, TELEOP_4, ENDGAME;
     }
 
+    private final EventLoop eventLoop = new EventLoop();
+    private final BooleanEvent allianceSetEvent;
     private Phase phase = Constants.GameInfoConstants.START_PHASE;
     private Alliance alliance = Constants.GameInfoConstants.DEFAULT_ALLIANCE;
+    private boolean isDistinctAlliance = false;
+    private boolean hasGotAlliance = false;
 
     /**
      * Subsystem for getting game info.
      */
-    public GameInfo() {}
+    public GameInfo() {
+        BooleanEvent isDistinctAllianceEvent = new BooleanEvent(eventLoop, () -> isDistinctAlliance);
+        BooleanEvent hasGotAllianceEvent = new BooleanEvent(eventLoop, () -> hasGotAlliance);
+        this.allianceSetEvent = isDistinctAllianceEvent
+                .rising()
+                .or(hasGotAllianceEvent.rising());
+    }
 
     /**
      * Gets the current phase.
@@ -45,10 +57,15 @@ public class GameInfo extends SubsystemBase {
      * Update the alliance to match the driver station.
      */
     private void updateAlliance() {
-        Optional<Alliance> alliance_option = DriverStation.getAlliance();
+        Optional<Alliance> newAllianceOption = DriverStation.getAlliance();
 
-        if (alliance_option.isPresent()) {
-            alliance = alliance_option.get();
+        if (newAllianceOption.isPresent()) {
+            Alliance newAlliance = newAllianceOption.get();
+
+            isDistinctAlliance = newAlliance != alliance;
+            hasGotAlliance = true;
+
+            alliance = newAlliance;
         }
     }
 
@@ -61,6 +78,19 @@ public class GameInfo extends SubsystemBase {
     public void periodic() {
         if (RobotState.isDisabled()) {
             updateAlliance();
+        } else {
+            isDistinctAlliance = false;
         }
+
+        eventLoop.poll();
+    }
+
+    /**
+     * Bind the action for when an alliance has been set by the driver station.
+     * 
+     * @param action the action.
+     */
+    public void onAllianceSet(Runnable action) {
+        allianceSetEvent.ifHigh(action);
     }
 }
