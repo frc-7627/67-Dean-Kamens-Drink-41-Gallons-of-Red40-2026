171935f9ff5471b762c111e3728e01895216da2c
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-23 08:42:28 -0800

    Added initAll and updateAll.

--- src/main/java/frc/robot/subsystems/util/dashboard/DashboardField.java
+++ src/main/java/frc/robot/subsystems/util/dashboard/DashboardField.java
@@ -4,4 +4,16 @@ public interface DashboardField {
     void init();
 
     void update();
+
+    static void initAll(DashboardField[] dashboardFields) {
+        for (DashboardField dashboardField : dashboardFields) {
+            dashboardField.init();
+        }
+    }
+
+    static void updateAll(DashboardField[] dashboardFields) {
+        for (DashboardField dashboardField : dashboardFields) {
+            dashboardField.update();
+        }
+    }
 }
