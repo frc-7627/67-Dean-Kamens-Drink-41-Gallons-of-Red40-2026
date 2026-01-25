ad96ed7fb81b8bea045537ec31cdcb3cf087a660
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-25 12:10:39 -0800

    Added constructor

--- src/main/java/frc/robot/subsystems/swervedrive/vision/NoCameraConnection.java
+++ src/main/java/frc/robot/subsystems/swervedrive/vision/NoCameraConnection.java
@@ -1,5 +1,7 @@
 package frc.robot.subsystems.swervedrive.vision;
 
 public class NoCameraConnection extends Exception {
-
+    public NoCameraConnection(String message) {
+        super(message);
+    }
 }
