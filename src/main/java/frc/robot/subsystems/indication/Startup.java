d8f5b34ff6a3d28eb1ef3d62aad7a010daf95901
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-25 10:47:33 -0800

    Added Startup class.

---
+++ src/main/java/frc/robot/subsystems/indication/Startup.java
@@ -0,0 +1,46 @@
+package frc.robot.subsystems.indication;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.logging.Logger;
+import static frc.robot.Constants.StartupConstants.*;
+
+public class Startup {
+    private static final Logger LOGGER = Logger.getLogger(Startup.class.getSimpleName());
+
+    /**
+     * Startup messages.
+     * 
+     * Displays the startup banner and hits the griddy on the ops.
+     */
+    public void startup() {
+        displayStartupBanner();
+        hitGriddyOnTheOps();
+    }
+
+    /**
+     * Display the startup banner.
+     */
+    private void displayStartupBanner() {
+        try {
+            final String startupBanner = Files.readString(Path.of(STARTUP_BANNER_FILE_PATH));
+
+            System.out.println(startupBanner);
+        } catch (FileNotFoundException exception) {
+            LOGGER.severe(String.format(
+                    "Could not display startup banner because the file at '%s' was not found!",
+                    STARTUP_BANNER_FILE_PATH));
+        } catch (IOException exception) {
+            LOGGER.severe("Could not close startup banner file!");
+        }
+    }
+
+    /**
+     * Hit the griddy on the ops.
+     */
+    private void hitGriddyOnTheOps() {
+        System.out.println("Hitting the griddy on the ops since 7627.");
+    }
+}
