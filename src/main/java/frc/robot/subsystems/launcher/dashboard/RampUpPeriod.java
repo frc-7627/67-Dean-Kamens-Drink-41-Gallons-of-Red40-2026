0350ccc5707180cf5d28733f40526b52ad75771f
Elizabeth <lopez.elizabeth.alica@gmail.com>
2026-01-27 15:15:24 -0800

    Added timers to config calls to min can overload

--- src/main/java/frc/robot/subsystems/launcher/dashboard/RampUpPeriod.java
+++ src/main/java/frc/robot/subsystems/launcher/dashboard/RampUpPeriod.java
@@ -1,6 +1,9 @@
 package frc.robot.subsystems.launcher.dashboard;
 
+import static edu.wpi.first.units.Units.Milliseconds;
 import static frc.robot.Constants.LauncherConstants.DEFAULT_RAMP_UP_PERIOD;
+
+import edu.wpi.first.wpilibj.Timer;
 import frc.robot.subsystems.launcher.Launcher;
 import frc.robot.subsystems.launcher.util.MotorsConfigurator;
 import frc.robot.subsystems.util.dashboard.DashboardDouble;
@@ -9,17 +12,24 @@ import frc.robot.subsystems.util.dashboard.FieldMode;
 public class RampUpPeriod extends DashboardDouble {
     private static final String FIELD_NAME = "Ramp Up Period";
     private final MotorsConfigurator configurator;
+    private final Timer timer = new Timer();
 
     public RampUpPeriod(MotorsConfigurator configurator) {
         super(Launcher.class.getSimpleName(), FIELD_NAME, DEFAULT_RAMP_UP_PERIOD, FieldMode.PULL);
 
         this.configurator = configurator;
+
+        timer.start();
     }
 
     @Override
     public void setInnerValue(Double innerValue) {
         super.setInnerValue(innerValue);
 
-        configurator.applyRampUpPeriod(getInnerValue());
+        if (timer.hasElapsed(Milliseconds.of(15))) { // TODO create constant
+            configurator.applyRampUpPeriod(getInnerValue());
+
+            timer.restart();
+        }
     }
 }
