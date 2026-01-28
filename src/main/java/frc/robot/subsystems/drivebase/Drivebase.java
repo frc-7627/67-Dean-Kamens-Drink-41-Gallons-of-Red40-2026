826e62669e61f388353a849188948def45a86681
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-27 20:26:50 -0800

    Added DrivebaseMarker

--- src/main/java/frc/robot/subsystems/drivebase/Drivebase.java
+++ src/main/java/frc/robot/subsystems/drivebase/Drivebase.java
@@ -1,10 +1,9 @@
 package frc.robot.subsystems.drivebase;
 
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.Subsystem;
 import com.pathplanner.lib.path.PathConstraints;
 
-public interface Drivebase extends Subsystem {
+public interface Drivebase extends DrivebaseMarker {
     void lock();
 
     void zeroGyro();
---
+++ src/main/java/frc/robot/subsystems/drivebase/DrivebaseMarker.java
@@ -0,0 +1,7 @@
+package frc.robot.subsystems.drivebase;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public interface DrivebaseMarker extends Subsystem {
+
+}
