ac4a44425e754835586896bc97223c4ed6cda8cc
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-27 20:43:41 -0800

    Added Cameras

---
+++ src/main/java/frc/robot/subsystems/vision/Cameras.java
@@ -0,0 +1,22 @@
+package frc.robot.subsystems.vision;
+
+import java.util.List;
+
+public class Cameras implements Vision {
+    Cameras() {
+        
+    }
+
+    @Override
+    public Target getBestTarget() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBestTarget'");
+    }
+
+    @Override
+    public List<VisionMeasurement> getVisionMeasurements() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getVisionMeasurements'");
+    }
+    
+}
