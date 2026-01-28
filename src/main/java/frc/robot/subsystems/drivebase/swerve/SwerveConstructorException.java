d2cfcd8b0a8e9b415f5eee0399a7a658adf885d2
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-27 21:36:21 -0800

    Removed Swerve and related

--- src/main/java/frc/robot/subsystems/drivebase/swerve/SwerveConstructorException.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.subsystems.drivebase.swerve;
-
-public class SwerveConstructorException extends Exception {
-    SwerveConstructorException(String msg, Throwable cause) {
-        super(msg, cause);
-    }
-}
