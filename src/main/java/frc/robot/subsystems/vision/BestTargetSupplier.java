48d2972f0cad52380b9daaa85bd1b7d9798cbac5
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-27 22:13:55 -0800

    Added getBestTargetPose to BestTargetSupplier

--- src/main/java/frc/robot/subsystems/vision/BestTargetSupplier.java
+++ src/main/java/frc/robot/subsystems/vision/BestTargetSupplier.java
@@ -1,5 +1,12 @@
 package frc.robot.subsystems.vision;
 
+import java.util.Optional;
+import edu.wpi.first.math.geometry.Pose2d;
+
 public interface BestTargetSupplier {
     Target getBestTarget();
+
+    default Optional<Pose2d> getBestTargetPose() {
+        return getBestTarget().getPose();
+    }
 }
