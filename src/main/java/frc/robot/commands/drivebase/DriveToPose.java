713ba0d9c3fac3c128513079e7850fb28e14665c
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-28 16:17:55 -0800

    Renamed PathConstraintsSupplier to AutoDrive

--- src/main/java/frc/robot/commands/drivebase/DriveToPose.java
+++ src/main/java/frc/robot/commands/drivebase/DriveToPose.java
@@ -5,10 +5,10 @@ import com.pathplanner.lib.auto.AutoBuilder;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.WrapperCommand;
-import frc.robot.subsystems.drivebase.PathConstraintsSupplier;
+import frc.robot.subsystems.drivebase.AutoDrive;
 
 public class DriveToPose extends WrapperCommand {
-    public DriveToPose(PathConstraintsSupplier drivebase,
+    public DriveToPose(AutoDrive drivebase,
             Optional<Pose2d> targetPoseOptional) {
         super(targetPoseOptional.isPresent() ? AutoBuilder.pathfindToPose(targetPoseOptional.get(),
                 drivebase.getPathConstraints()) : Commands.none());
--- src/main/java/frc/robot/subsystems/drivebase/AutoDrive.java
+++ src/main/java/frc/robot/subsystems/drivebase/AutoDrive.java
@@ -3,6 +3,6 @@ package frc.robot.subsystems.drivebase;
 import com.pathplanner.lib.path.PathConstraints;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
-public interface PathConstraintsSupplier extends Subsystem {
+public interface AutoDrive extends Subsystem {
     PathConstraints getPathConstraints();
 }
