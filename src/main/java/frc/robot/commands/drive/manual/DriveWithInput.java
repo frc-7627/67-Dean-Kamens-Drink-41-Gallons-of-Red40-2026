45c5999f0966dd76175e0ce671f64fbba54a864e
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-28 17:25:22 -0800

    Simplified

--- src/main/java/frc/robot/commands/drive/manual/DriveWithInput.java
+++ src/main/java/frc/robot/commands/drive/manual/DriveWithInput.java
@@ -2,20 +2,11 @@ package frc.robot.commands.drive.manual;
 
 import java.util.function.Supplier;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.RunCommand;
 import frc.robot.subsystems.drivebase.ManualDrivebase;
 
-public class DriveWithInput extends Command {
-    private final ManualDrivebase drivebase;
-    private final Supplier<ChassisSpeeds> input;
-
+public class DriveWithInput extends RunCommand {
     public DriveWithInput(ManualDrivebase drivebase, Supplier<ChassisSpeeds> input) {
-        this.drivebase = drivebase;
-        this.input = input;
-    }
-
-    @Override
-    public void execute() {
-        drivebase.driveWithSpeeds(input.get());
+        super(() -> drivebase.driveWithSpeeds(input.get()), drivebase);
     }
 }
