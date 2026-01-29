4ec37b7feff6d82f84962e6ad22fab6b08f3aa66
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-28 21:15:08 -0800

    Added Resource

---
+++ src/main/java/frc/robot/resources/Resource.java
@@ -0,0 +1,15 @@
+package frc.robot.resources;
+
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.RunCommand;
+
+/**
+ * A shared resource that may have periodic behavior.
+ */
+public abstract class Resource {
+    protected Resource() {
+        CommandScheduler.getInstance().schedule(new RunCommand(this::periodic));
+    }
+
+    protected void periodic() {}
+}
