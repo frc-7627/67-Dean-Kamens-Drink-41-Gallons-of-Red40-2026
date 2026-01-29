c43b1a6c983f6c89b1399cf4f5ba5c42a1bad60b
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-28 21:31:59 -0800

    Added factory method

--- src/main/java/frc/robot/resources/gameinfo/GameInfoSupplier.java
+++ src/main/java/frc/robot/resources/gameinfo/GameInfoSupplier.java
@@ -1,5 +1,7 @@
 package frc.robot.resources.gameinfo;
 
 public interface GameInfoSupplier extends GeneralGameInfoSupplier, SpecificGameInfoSupplier {
-    
+    static GameInfoSupplier create() {
+        throw new UnsupportedOperationException("Game info supplier not implemented!");
+    }
 }
