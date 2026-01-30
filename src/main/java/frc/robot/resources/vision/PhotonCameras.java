e771ce052930783094465688b33b2c76cd5be267
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-29 17:10:47 -0800

    Implemented PhotonCameras

--- src/main/java/frc/robot/resources/vision/PhotonCameras.java
+++ src/main/java/frc/robot/resources/vision/PhotonCameras.java
@@ -1,23 +1,52 @@
 package frc.robot.resources.vision;
 
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
 import frc.robot.resources.Resource;
+import frc.robot.subsystems.legacy.vision.dashboard.StandardDeviations;
+import frc.robot.subsystems.util.dashboard.DashboardField;
 
 class PhotonCameras extends Resource implements Vision {
+    private final List<PhotonCameraWrapper> photonCameraWrappers;
+
+    private final StandardDeviations standardDeviations = new StandardDeviations();
+
+    private final DashboardField[] dashboardFields = {standardDeviations,};
+
     PhotonCameras() throws VisionInitException {
+        this.photonCameraWrappers = List.of(PhotonCameraInfo.values()).stream()
+                .map(photonCameraInfo -> new PhotonCameraWrapper(photonCameraInfo)).toList();
 
+        DashboardField.initAll(dashboardFields);
     }
 
     @Override
-    public Target getBestTarget() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getBestTarget'");
+    protected void periodic() {
+        DashboardField.updateAll(dashboardFields);
+    }
+
+    /**
+     * @return all targets from all cameras.
+     */
+    private Stream<PhotonTrackedTargetWrapper> getBestTargets() {
+        return photonCameraWrappers.stream()
+                .map(photonCameraWrapper -> photonCameraWrapper.getLatestResult())
+                .map(resultOptional -> resultOptional.map(result -> result.getBestTarget()))
+                .flatMap(bestTargetOptional -> bestTargetOptional.stream())
+                .map(photonTrackedTarget -> new PhotonTrackedTargetWrapper(photonTrackedTarget));
     }
 
     @Override
-    public List<VisionMeasurement> getVisionMeasurements() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'getVisionMeasurements'");
+    public Optional<Target> getBestTarget() {
+        return getBestTargets().max((a, b) -> a.compareWith(b))
+                .map(comparableTarget -> (Target) comparableTarget);
+    }
+
+    @Override
+    public Stream<VisionMeasurement> getVisionMeasurements() {
+        return photonCameraWrappers.stream().flatMap(photonCameraWrapper -> photonCameraWrapper
+                .getVisionMeasurement(standardDeviations).stream());
     }
 
 }
