a1229e204b3ad7d1366a9b388efb987d79a41a83
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-30 13:17:35 -0800

    Added some commonly used dashboard fields

---
+++ src/main/java/frc/robotlib/resource/dashboard/fields/StoringDouble.java
@@ -0,0 +1,28 @@
+package frc.robotlib.resource.dashboard.fields;
+
+import java.util.function.Consumer;
+import java.util.function.Predicate;
+
+public class StoringDouble extends StoringFieldBase<Double> {
+    public StoringDouble(String superdashboardName, String fieldName, Predicate<Double> checkStored,
+            Consumer<Double> onStore, Double initialDouble) {
+        super(superdashboardName, fieldName, Transport::createDouble, checkStored, onStore,
+                initialDouble);
+    }
+
+    public StoringDouble(String superdashboardName, String fieldName, Consumer<Double> onStore,
+            Double initialDouble) {
+        this(superdashboardName, fieldName, stored -> true, onStore, initialDouble);
+    }
+
+    public StoringDouble(String superdashboardName, String fieldName, Predicate<Double> checkStored,
+            Double initialDouble) {
+        this(superdashboardName, fieldName, checkStored, stored -> {
+        }, initialDouble);
+    }
+
+    public StoringDouble(String superdashboardName, String fieldName, Double initialDouble) {
+        this(superdashboardName, fieldName, stored -> true, stored -> {
+        }, initialDouble);
+    }
+}
