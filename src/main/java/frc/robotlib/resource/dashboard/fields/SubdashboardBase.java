7209c32ccb41c94b203995fbb246d12fdc3b78ca
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-30 10:59:08 -0800

    Renamed stuff in DashboardFieldBase to SubdashboardBase.

---
+++ src/main/java/frc/robotlib/resource/dashboard/fields/SubdashboardBase.java
@@ -0,0 +1,23 @@
+package frc.robotlib.resource.dashboard.fields;
+
+import frc.robotlib.resource.dashboard.Subdashboard;
+
+abstract class SubdashboardBase implements Subdashboard {
+    private final String superdashboardName;
+    private final String subdashboardName;
+
+    protected SubdashboardBase(String superdashboardName, String subdashboardName) {
+        this.superdashboardName = superdashboardName;
+        this.subdashboardName = subdashboardName;
+    }
+
+    @Override
+    public final String getSuperdashboardName() {
+        return superdashboardName;
+    }
+
+    @Override
+    public final String getSubdashboardName() {
+        return subdashboardName;
+    }
+}
