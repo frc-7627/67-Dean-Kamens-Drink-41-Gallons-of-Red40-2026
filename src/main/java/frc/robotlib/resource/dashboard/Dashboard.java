99328fb6f7118015465c903cb12f474765c2a274
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-30 13:37:54 -0800

    Dashboard gives itself a proper name.

--- src/main/java/frc/robotlib/resource/dashboard/Dashboard.java
+++ src/main/java/frc/robotlib/resource/dashboard/Dashboard.java
@@ -6,6 +6,11 @@ import frc.robotlib.resource.Resource;
 public interface Dashboard extends Resource {
     String getDashboardName();
 
+    @Override
+    default String getName() {
+        return getDashboardName() + ".Dashboard";
+    }
+
     static Dashboard create(String dashboardName,
             Collection<? extends Subdashboard> subdashboards) {
         return new DashboardImpl(dashboardName, subdashboards);
