2dd882cac8310a8a002199973a929b69e22a6e96
Elizabeth <lopez.elizabeth.alica@gmail.com>
2026-01-29 17:16:47 -0800

    Added some more comments

--- src/main/java/frc/robot/resources/vision/BestTargetSupplier.java
+++ src/main/java/frc/robot/resources/vision/BestTargetSupplier.java
@@ -4,6 +4,15 @@ import java.util.Optional;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public interface BestTargetSupplier {
+
+    /**
+     * Returns the best target in this pipeline result. If there are no targets,
+     * this method will
+     * return null. The best target is determined by the target sort mode in the
+     * PhotonVision UI.
+     *
+     * @return The best target of the pipeline result.
+     */
     Target getBestTarget();
 
     default Optional<Pose2d> getBestTargetPose() {
--- src/main/java/frc/robot/resources/vision/Target.java
+++ src/main/java/frc/robot/resources/vision/Target.java
@@ -4,5 +4,12 @@ import java.util.Optional;
 import edu.wpi.first.math.geometry.Pose2d;
 
 public interface Target {
+
+    /**
+     * Gets the measured pose (position and rotation) of the robot, as reported by
+     * odometry.
+     *
+     * @return The robot's pose
+     */
     Optional<Pose2d> getPose();
 }
