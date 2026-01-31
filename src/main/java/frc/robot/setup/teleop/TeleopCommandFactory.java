050e19d1a4b5c1bb5b92a3dfad5ca74492109067
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-31 12:32:22 -0800

    Import cleanup

--- src/main/java/frc/robot/setup/teleop/TeleopCommandFactory.java
+++ src/main/java/frc/robot/setup/teleop/TeleopCommandFactory.java
@@ -4,7 +4,6 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.PrintCommand;
 import frc.robot.commands.LoggingWrapperCommand;
 import frc.robot.commands.MockCommand;
 import frc.robot.commands.control.ToggleControlState;
