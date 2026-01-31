0c3966c4504443367fa22d1a3f7cf348c6283c5a
Gianmarco Lenzi <tyedee09@gmail.com>
2026-01-31 10:05:36 -0800

    Implemented global control state

--- src/main/java/frc/robot/subsystems/controlstate/GlobalControlStateImpl.java
+++ src/main/java/frc/robot/subsystems/controlstate/GlobalControlStateImpl.java
@@ -1,17 +1,27 @@
 package frc.robot.subsystems.controlstate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 class GlobalControlStateImpl extends SubsystemBase implements GlobalControlState {
+    private final List<Consumer<ControlState>> listeners = new ArrayList<>();
+
     private ControlState controlState = ControlState.NORMAL;
 
     @Override
-    public void toggleControlState() {}
+    public void toggleControlState() {
+        switch (controlState) {
+            case NORMAL -> controlState = ControlState.MANUAL;
+            case MANUAL -> controlState = ControlState.NORMAL;
+        }
+
+        listeners.forEach(listener -> listener.accept(controlState));
+    }
 
     @Override
     public void onNewControlState(Consumer<ControlState> action) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'onNewControlState'");
+        listeners.add(action);
     }
 }
