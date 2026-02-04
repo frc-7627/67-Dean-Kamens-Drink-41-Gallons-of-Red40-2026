456cc9ae6067500363782adaef8c2829c89843e2
Ryan Wang <rryanwwang@gmail.com>
2026-02-03 17:44:30 -0800

    finished documenting all of the interfaces

--- src/main/java/frc/robot/subsystems/drivebase/SemidirectDrivebase.java
+++ src/main/java/frc/robot/subsystems/drivebase/SemidirectDrivebase.java
@@ -1,5 +1,8 @@
 package frc.robot.subsystems.drivebase;
 
+/**
+ * Interface managing the rotation of the robot along with the motion.
+ */
 public interface SemidirectDrivebase extends DirectDrivebase, KinematicSupplier {
     AngularControl getAngularControl();
 }
