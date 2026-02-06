4c6e11d1e4d1d166e3a490126d79f30df815529d
Ryan Wang <rryanwwang@gmail.com>
2026-02-05 17:52:41 -0800

    began implementing feeder

--- src/main/java/frc/robot/subsystems/feeder/Feeder.java
+++ src/main/java/frc/robot/subsystems/feeder/Feeder.java
@@ -6,6 +6,21 @@ import edu.wpi.first.wpilibj2.command.Subsystem;
  * Interface for the feeder mechanism of the robot.
  */
 public interface Feeder extends Subsystem {
+    /**
+     * Turn on the conveyor to move the gamepiece from the intake to the hopper.
+     */
+    void feedIn();
+
+    /**
+     * Feed the opposite way.
+     */
+    void feedOut();
+
+    /**
+     * Stop the feeder.
+     */
+    void stop();
+
     static Feeder create() {
         return new FeederImpl();
     }
