c3a1a78a96b58c2dcfec321fbea7e76892781216
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-05 20:48:43 -0800

    Removed unneeded file.

--- src/main/java/frc/robot/subsystems/drivebase/FieldRelativeDrivebase.java
+++
@@ -1,8 +0,0 @@
-package frc.robot.subsystems.drivebase;
-
-import edu.wpi.first.math.kinematics.ChassisSpeeds;
-import edu.wpi.first.wpilibj2.command.Subsystem;
-
-public interface FieldRelativeDrivebase extends Subsystem {
-    void driveWithFieldRelativeSpeeds(ChassisSpeeds fieldRelativeSpeeds);
-}
