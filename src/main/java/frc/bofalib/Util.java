eb0b2171e7da15890fb484ddda16ed7fe14d73eb
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-07 13:27:12 -0800

    Removed old throttle methods

--- src/main/java/frc/bofalib/Util.java
+++ src/main/java/frc/bofalib/Util.java
@@ -8,26 +8,4 @@ public final class Util {
     public static String mockName(String name) {
         return name + " (mock)";
     }
-
-    public static Runnable throttle(Runnable action, Frequency frequency) {
-        return throttle(action, frequency.asPeriod());
-    }
-
-    public static Runnable throttle(Runnable action, Time period) {
-        return new Runnable() {
-            private final Timer timer = new Timer();
-
-            {
-                timer.start();
-            }
-
-            @Override
-            public void run() {
-                if (timer.hasElapsed(period)) {
-                    action.run();
-                    timer.reset();
-                }
-            }
-        };
-    }
 }
