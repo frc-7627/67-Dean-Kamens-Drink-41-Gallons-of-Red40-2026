fa1aec5c68d42ea7376dae45b736f890f2f29228
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-07 12:18:24 -0800

    New dashboard stuff in feeder

--- src/main/java/frc/robot/subsystems/feeder/FeederImpl.java
+++ src/main/java/frc/robot/subsystems/feeder/FeederImpl.java
@@ -1,38 +1,53 @@
 package frc.robot.subsystems.feeder;
 
 import static frc.robot.Constants.FeederConstants.*;
+import java.util.function.DoubleSupplier;
 import static frc.robot.Constants.CHECK_SIMPLE_MOTOR_SPEED;
-
+import static frc.robot.Constants.MOTOR_CONFIGURE_FREQUENCY;
 import com.ctre.phoenix6.configs.CurrentLimitsConfigs;
 import com.ctre.phoenix6.configs.MotorOutputConfigs;
 import com.ctre.phoenix6.hardware.TalonFX;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import frc.bofalib.dashboard.fields.PullingDouble;
+import frc.bofalib.Util;
+import frc.bofalib.dashboard.DashboardItems;
+import frc.bofalib.dashboard.KeyBuilder;
 
 // Colloquially known as The Berlin Wall.
 class FeederImpl extends SubsystemBase implements Feeder {
 
     // 1 kraken
-
-    private static final String DASHBOARD_NAME = Feeder.class.getName();
+    private static final KeyBuilder KEY_BUILDER = KeyBuilder.of(Feeder.class.getSimpleName());
     private final TalonFX feederMotor = new TalonFX(0);
 
     private final CurrentLimitsConfigs currentLimitsConfigs = DEFAULT_CURRENT_LIMITS_CONFIGS;
     private final MotorOutputConfigs motorOutputConfigs = DEFAULT_MOTOR_OUTPUT_CONFIGS;
+    
+    private final DoubleSupplier currentLimitSupplier = DashboardItems.createDoublePuller(
+        KEY_BUILDER.copyExtendedToString("Current Limit"), 
+        DEFAULT_CURRENT_LIMIT
+    );
 
-    private final PullingDouble currentLimit = new PullingDouble(
-            DASHBOARD_NAME,
-            "Current Limit",
-            this::applyCurrentLimit,
-            DEFAULT_CURRENT_LIMIT);
+    private final DoubleSupplier feedSpeedSupplier = DashboardItems.createDoublePuller(
+        KEY_BUILDER.copyExtendedToString("Feed Speed"), 
+        DEFAULT_FEED_SPEED,
+        CHECK_SIMPLE_MOTOR_SPEED
+    );
 
-    private final PullingDouble feedSpeed = new PullingDouble(
-            DASHBOARD_NAME,
-            "Feed Speed",
-            CHECK_SIMPLE_MOTOR_SPEED,
-            this::applyFeedSpeed,
-            DEFAULT_FEED_SPEED);
+    @Override
+    public void periodic() {
+        Util.throttle(() -> {
+            apply(
+                currentLimitSupplier.getAsDouble(), 
+                feedSpeedSupplier.getAsDouble()
+            );
+        }, MOTOR_CONFIGURE_FREQUENCY);
+    }
+
+    private void apply(double currentLimit, double feedSpeed) {
+        applyCurrentLimit(currentLimit);
+        applyFeedSpeed(feedSpeed);
+    }
 
     void applyCurrentLimit(double currentLimit) {
         currentLimitsConfigs.withStatorCurrentLimit(currentLimit);
@@ -45,12 +60,12 @@ class FeederImpl extends SubsystemBase implements Feeder {
 
     @Override
     public void feedIn() {
-        feederMotor.set(feedSpeed.getPulled());
+        feederMotor.set(feedSpeedSupplier.getAsDouble());
     }
 
     @Override
     public void feedOut() {
-        feederMotor.set(-feedSpeed.getPulled());
+        feederMotor.set(-feedSpeedSupplier.getAsDouble());
     }
 
     /**
