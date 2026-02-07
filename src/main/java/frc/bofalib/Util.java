ed0b6f02c1c35a03b0ee0c118e56ba73429318e5
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-07 13:27:21 -0800

    Cleaned imports

--- src/main/java/frc/bofalib/Util.java
+++ src/main/java/frc/bofalib/Util.java
@@ -1,9 +1,5 @@
 package frc.bofalib;
 
-import edu.wpi.first.units.measure.Frequency;
-import edu.wpi.first.units.measure.Time;
-import edu.wpi.first.wpilibj.Timer;
-
 public final class Util {
     public static String mockName(String name) {
         return name + " (mock)";
