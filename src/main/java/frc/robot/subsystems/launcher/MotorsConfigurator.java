91d6ce8943345d24b2eb28a0ca48ef46fd6cf232
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-07 11:59:10 -0800

    Added apply method

--- src/main/java/frc/robot/subsystems/launcher/MotorsConfigurator.java
+++ src/main/java/frc/robot/subsystems/launcher/MotorsConfigurator.java
@@ -53,19 +53,6 @@ final class MotorsConfigurator {
     }
 
     /**
-     * Update the configuration with the new shoot speed.
-     * 
-     * @param shootSpeed the new shoot speed.
-     */
-    void applyShootSpeed(double shootSpeed) {
-        motorOutputConfigs.withPeakForwardDutyCycle(shootSpeed);
-        motorOutputConfigs.withPeakReverseDutyCycle(-shootSpeed);
-
-        commanderConfigurator.apply(motorOutputConfigs);
-        minionConfigurator.apply(motorOutputConfigs);
-    }
-
-    /**
      * Update the configuration with the new ramp up period.
      * 
      * @param rampUpPeriod the new ramp up period.
@@ -79,5 +66,24 @@ final class MotorsConfigurator {
 
         minionConfigurator.apply(openLoopRampsConfigs);
         minionConfigurator.apply(closedLoopRampsConfigs);
+    }
+
+    /**
+     * Update the configuration with the new shoot speed.
+     * 
+     * @param shootSpeed the new shoot speed.
+     */
+    void applyShootSpeed(double shootSpeed) {
+        motorOutputConfigs.withPeakForwardDutyCycle(shootSpeed);
+        motorOutputConfigs.withPeakReverseDutyCycle(-shootSpeed);
+
+        commanderConfigurator.apply(motorOutputConfigs);
+        minionConfigurator.apply(motorOutputConfigs);
+    }
+
+    void apply(double currentLimit, double rampUpPeriod, double shootSpeed) {
+        applyCurrentLimit(currentLimit);
+        applyRampUpPeriod(rampUpPeriod);
+        applyShootSpeed(shootSpeed);
     }
 }
