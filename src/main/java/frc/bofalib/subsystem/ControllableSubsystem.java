e294e61bf1a4e9c15b91fb9b65613d2674f93a54
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-10 11:04:55 -0800

    Some docs

--- src/main/java/frc/bofalib/subsystem/ControllableSubsystem.java
+++ src/main/java/frc/bofalib/subsystem/ControllableSubsystem.java
@@ -2,10 +2,26 @@ package frc.bofalib.subsystem;
 
 import edu.wpi.first.wpilibj2.command.Subsystem;
 
+/**
+ * Subsystems that can be controlled using a control strategy.
+ * 
+ * @param <Control> control strategies
+ */
 public interface ControllableSubsystem<Control> extends Subsystem {
+    /**
+     * Sets the control strategy to the provided control strategy.
+     * 
+     * @param control the provided control strategy.
+     */
     void beginControl(Control control);
 
+    /**
+     * Execute the control strategy.
+     */
     void runControl();
 
+    /**
+     * Do any required work to cleanup using the control strategy.
+     */
     default void endControl() {}
 }
--- src/main/java/frc/robot/commands/ControlCommand.java
+++ src/main/java/frc/robot/commands/ControlCommand.java
@@ -3,10 +3,23 @@ package frc.robot.commands;
 import edu.wpi.first.wpilibj2.command.FunctionalCommand;
 import frc.bofalib.subsystem.ControllableSubsystem;
 
+/**
+ * Commands that continuously control a target subsystem using a control strategy.
+ * 
+ * @param <Control> control strategies
+ * @param <TargetSubsystem> target subsystems
+ */
 public final class ControlCommand<
     Control, 
     TargetSubsystem extends ControllableSubsystem<Control>
 > extends FunctionalCommand {
+    /**
+     * Create a command that continuously controls the target subsystem using the provided control
+     * strategy.
+     * 
+     * @param targetSubsystem the target subsystem
+     * @param control the provided control strategy
+     */
     public ControlCommand(TargetSubsystem targetSubsystem, Control control) {
         super(
             () -> targetSubsystem.beginControl(control), 
