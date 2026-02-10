b7c1a9e18e203951f335158c088418a6c0681b71
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-10 10:42:26 -0800

    Removed unneeded commands

--- src/main/java/frc/robot/commands/drive/direct/DriveAngular.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands.drive.direct;
-
-import frc.robot.subsystems.drivebase.SemidirectDrivebase;
-
-abstract class DriveAngular extends DriveSemidirect {
-    protected DriveAngular(SemidirectDrivebase drivebase, OrientationTarget target) {
-        super(drivebase, target);
-    }
-}
--- src/main/java/frc/robot/commands/drive/direct/OrientationTarget.java
+++
@@ -1,9 +0,0 @@
-package frc.robot.commands.drive.direct;
-
-import edu.wpi.first.units.measure.Angle;
-
-abstract class OrientationTarget {
-    abstract Angle getOrientationAngle();
-
-    void initialize() {}
-}
