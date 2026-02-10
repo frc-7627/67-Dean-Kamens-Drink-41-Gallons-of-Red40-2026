335bed98e309c08e6d370d51fe06bf19f6f5ae68
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-10 10:30:14 -0800

    Path planner config not in auto chooser

--- src/main/java/frc/robot/setup/auto/AutoChooser.java
+++ src/main/java/frc/robot/setup/auto/AutoChooser.java
@@ -2,15 +2,12 @@ package frc.robot.setup.auto;
 
 import java.util.function.Supplier;
 import edu.wpi.first.wpilibj2.command.Command;
-import frc.robot.subsystems.pathplanner.PathPlannerConfigException;
-import frc.robot.subsystems.pathplanner.PathPlannerConfigurator;
 
 /**
  * Interface for selecting the autonomous commands
  */
 public interface AutoChooser extends Supplier<Command> {
-    static AutoChooser create(
-            PathPlannerConfigurator configurator) throws PathPlannerConfigException {
-        return new AutoChooserImpl(configurator);
+    static AutoChooser create() {
+        return new AutoChooserImpl();
     }
 }
--- src/main/java/frc/robot/setup/auto/AutoChooserImpl.java
+++ src/main/java/frc/robot/setup/auto/AutoChooserImpl.java
@@ -6,15 +6,11 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.pathplanner.PathPlannerConfigException;
-import frc.robot.subsystems.pathplanner.PathPlannerConfigurator;
 
 final class AutoChooserImpl implements AutoChooser {
     private final SendableChooser<Command> chooser;
 
-    AutoChooserImpl(PathPlannerConfigurator configurator) throws PathPlannerConfigException {
-        configurator.configureAndInit();
-
+    AutoChooserImpl() {
         this.chooser = AutoBuilder.buildAutoChooser();
 
         setupChooser();
