4a2794abb22bed1ddb4f995429ffde63a07b61f7
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-14 14:47:24 -0800

    Added empty spark max control

---
+++ src/main/java/frc/bofalib/generic/hardware/motor/sparkmax/control/SparkMaxControlEmpty.java
@@ -0,0 +1,11 @@
+package frc.bofalib.generic.hardware.motor.sparkmax.control;
+
+public final class SparkMaxControlEmpty implements SparkMaxControl {
+    private static final SparkMaxControlEmpty INSTANCE = new SparkMaxControlEmpty();
+
+    public static SparkMaxControlEmpty getInstance() {
+        return INSTANCE;
+    }
+
+    private SparkMaxControlEmpty() {}
+}
