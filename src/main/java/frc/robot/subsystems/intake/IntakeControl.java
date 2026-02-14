6d913e6734f228b4aa63f6e99c0a5d1ac9deb94e
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-14 15:03:26 -0800

    Cleaned up intake a little

--- src/main/java/frc/robot/subsystems/intake/IntakeControl.java
+++ src/main/java/frc/robot/subsystems/intake/IntakeControl.java
@@ -13,38 +13,64 @@ import frc.bofalib.generic.hardware.motor.talon.control.TalonFXControlEmpty;
 import frc.bofalib.generic.hardware.motor.talon.control.TalonFXControlSetting;
 
 public enum IntakeControl implements BiControl<IntakeImpl, SparkMaxControl, TalonFXControl> {
-    LOAD(impl -> impl.intakeDutyCycle, false),
-    EJECT(impl -> BofaUtil.negativeSupplier(impl.intakeDutyCycle), false),
-    LOAD_MANUAL(impl -> impl.intakeManualDutyCycle, false),
-    EJECT_MANUAL(impl -> BofaUtil.negativeSupplier(impl.intakeManualDutyCycle), false),
-    FOLD_OUT(impl -> impl.foldDutyCycle, 0),
-    FOLD_IN(impl -> BofaUtil.negativeSupplier(impl.foldDutyCycle), 0);
+    LOAD(
+        Motor.INTAKE_MOTOR, 
+        impl -> impl.intakeDutyCycle
+    ),
+    EJECT(
+        Motor.INTAKE_MOTOR, 
+        impl -> BofaUtil.negativeSupplier(impl.intakeDutyCycle)
+    ),
+    LOAD_MANUAL(
+        Motor.INTAKE_MOTOR, 
+        impl -> impl.intakeManualDutyCycle
+    ),
+    EJECT_MANUAL(
+        Motor.INTAKE_MOTOR, 
+        impl -> BofaUtil.negativeSupplier(impl.intakeManualDutyCycle)
+    ),
+    FOLD_OUT(
+        Motor.PIVOT_MOTOR, 
+        impl -> impl.foldDutyCycle
+    ),
+    FOLD_IN(
+        Motor.PIVOT_MOTOR, 
+        impl -> BofaUtil.negativeSupplier(impl.foldDutyCycle)
+    );
+
+    private static enum Motor {
+        PIVOT_MOTOR,
+        INTAKE_MOTOR
+    }
 
     private final Function<IntakeImpl, SparkMaxControl> pivotControlFunction;
     private final Function<IntakeImpl, TalonFXControl> intakeControlFunction;
 
     private IntakeControl(
-        Function<IntakeImpl, DoubleSupplier> pivotDutyCycleFunction, 
-        int dummy
+        Motor motor,
+        Function<IntakeImpl, DoubleSupplier> dutyCycleFunction
     ) {
-        this.pivotControlFunction = impl -> new SparkMaxControlSetting(
-            new MotorDutyCycle(
-                pivotDutyCycleFunction.apply(impl)
-            )
-        );
-        this.intakeControlFunction = impl -> TalonFXControlEmpty.getInstance();
-    }
-
-    private IntakeControl(
-        Function<IntakeImpl, DoubleSupplier> intakeDutyCycleFunction,
-        boolean dummy
-    ) {
-        this.pivotControlFunction = impl -> SparkMaxControlEmpty.getInstance();
-        this.intakeControlFunction = impl -> new TalonFXControlSetting(
-            new MotorDutyCycle(
-                intakeDutyCycleFunction.apply(impl)
-            )
-        );
+        switch (motor) {
+            case PIVOT_MOTOR -> {
+                this.pivotControlFunction = impl -> new SparkMaxControlSetting(
+                    new MotorDutyCycle(
+                        dutyCycleFunction.apply(impl)
+                    )
+                );
+                this.intakeControlFunction = impl -> TalonFXControlEmpty.getInstance();
+            }
+            case INTAKE_MOTOR -> {
+                this.pivotControlFunction = impl -> SparkMaxControlEmpty.getInstance();
+                this.intakeControlFunction = impl -> new TalonFXControlSetting(
+                    new MotorDutyCycle(
+                        dutyCycleFunction.apply(impl)
+                    )
+                );
+            }
+            default -> {
+                throw new IllegalArgumentException();
+            }
+        }
     }
 
     @Override
