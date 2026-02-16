12903e6322710788416318ac18f73843097c2273
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-16 14:58:56 -0800

    Simplified gain item

--- src/main/java/frc/bofalib/gains/GainItem.java
+++ src/main/java/frc/bofalib/gains/GainItem.java
@@ -6,8 +6,36 @@ public final class GainItem {
     public final GainSelection selection;
     public final double defaultValue;
 
-    public GainItem(GainSelection selection, double defaultValue) {
+    private GainItem(GainSelection selection, double defaultValue) {
         this.selection = Objects.requireNonNull(selection);
         this.defaultValue = defaultValue;
+    }
+
+    public static GainItem createProportional(double defaultValue) {
+        return new GainItem(GainSelection.PROPORTIONAL, defaultValue);
+    }
+
+    public static GainItem createIntegral(double defaultValue) {
+        return new GainItem(GainSelection.INTEGRAL, defaultValue);
+    }
+
+    public static GainItem createDerivative(double defaultValue) {
+        return new GainItem(GainSelection.DERIVATIVE, defaultValue);
+    }
+
+    public static GainItem createStatic(double defaultValue) {
+        return new GainItem(GainSelection.STATIC, defaultValue);
+    }
+
+    public static GainItem createVelocity(double defaultValue) {
+        return new GainItem(GainSelection.VELOCITY, defaultValue);
+    }
+
+    public static GainItem createAcceleration(double defaultValue) {
+        return new GainItem(GainSelection.ACCELERATION, defaultValue);
+    }
+
+    public static GainItem createGravity(double defaultValue) {
+        return new GainItem(GainSelection.GRAVITY, defaultValue);
     }
 }
