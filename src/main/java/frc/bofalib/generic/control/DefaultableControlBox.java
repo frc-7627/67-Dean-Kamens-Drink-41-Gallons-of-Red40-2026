f346b0914e14a48208af2567ef81ee7e2ca5c222
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-17 15:58:30 -0800

    Added null check

--- src/main/java/frc/bofalib/generic/control/DefaultableControlBox.java
+++ src/main/java/frc/bofalib/generic/control/DefaultableControlBox.java
@@ -1,10 +1,13 @@
 package frc.bofalib.generic.control;
 
+import java.util.Objects;
+
 public final class DefaultableControlBox<Control> {
     private final Control emptyControl;
     private Control control;
 
     public DefaultableControlBox(Control emptyControl) {
+        Objects.requireNonNull(emptyControl);
         this.emptyControl = emptyControl;
         this.control = emptyControl;
     }
