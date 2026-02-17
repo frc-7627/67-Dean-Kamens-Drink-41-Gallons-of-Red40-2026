5ce3e68c27ee9676baba47f19825ca2be9b6dbc9
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-17 15:10:35 -0800

    Strengthened type parameter

--- src/main/java/frc/bofalib/generic/hardware/motor/MotorHardware.java
+++ src/main/java/frc/bofalib/generic/hardware/motor/MotorHardware.java
@@ -3,7 +3,7 @@ package frc.bofalib.generic.hardware.motor;
 import frc.bofalib.generic.hardware.motor.setting.MotorSetting;
 import frc.bofalib.hardware.Hardware;
 
-public interface MotorHardware<MotorControl, MotorConfig> extends 
+public interface MotorHardware<MotorControl, MotorConfig extends MotorConfigurator> extends 
     Hardware<MotorControl, MotorConfig>
 {
     MotorControl getSetControl(MotorSetting motorSetting);
