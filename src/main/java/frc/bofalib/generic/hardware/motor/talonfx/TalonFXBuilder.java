eb778d7516a7214365fc52a577abaed6445d0796
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-17 12:21:12 -0800

    Added builder and mock for individual talonfx

---
+++ src/main/java/frc/bofalib/generic/hardware/motor/talonfx/TalonFXBuilder.java
@@ -0,0 +1,68 @@
+package frc.bofalib.generic.hardware.motor.talonfx;
+
+import java.util.Optional;
+import java.util.OptionalInt;
+import com.ctre.phoenix6.configs.TalonFXConfiguration;
+
+public final class TalonFXBuilder {
+    private final String name;
+    private OptionalInt deviceIdOptional = OptionalInt.empty();
+    private OptionalInt trackNumberOptional = OptionalInt.empty();
+    private Optional<TalonFXConfiguration> configurationOptional = Optional.empty();
+
+    private TalonFXBuilder(String name) {
+        this.name = name;
+    }
+
+    private TalonFXBuilder(String name, int deviceId) {
+        this(name);
+
+        this.deviceIdOptional = OptionalInt.of(deviceId);
+    }
+
+    private boolean isMock() {
+        return deviceIdOptional.isPresent();
+    }
+
+    public static TalonFXBuilder create(String name, int deviceId) {
+        return new TalonFXBuilder(name, deviceId);
+    }
+
+    public static TalonFXBuilder mock(String name) {
+        return new TalonFXBuilder(name);
+    }
+
+    public static TalonFXBuilder mock(String name, int deviceId) {
+        return mock(name);
+    }
+
+    public TalonFXBuilder withTrackNumber(int trackNumber) {
+        this.trackNumberOptional = OptionalInt.of(trackNumber);
+
+        return this;
+    }
+
+    public TalonFXBuilder withConfig(TalonFXConfiguration configuration) {
+        this.configurationOptional = Optional.of(configuration);
+
+        return this;
+    }
+
+    public TalonFXWrapper build() {
+        final TalonFXWrapper wrapper = 
+            isMock() ? 
+            new TalonFXWrapperImpl(
+                name, 
+                deviceIdOptional.getAsInt(), 
+                trackNumberOptional
+            ) :
+            null
+        ;
+
+        configurationOptional.ifPresent(
+            wrapper.getConfigurator()::apply
+        );
+
+        return wrapper;
+    }    
+}
---
+++ src/main/java/frc/bofalib/generic/hardware/motor/talonfx/TalonFXWrapperMock.java
@@ -0,0 +1,54 @@
+package frc.bofalib.generic.hardware.motor.talonfx;
+
+import java.util.function.DoubleSupplier;
+import com.ctre.phoenix6.Orchestra;
+import frc.bofalib.generic.hardware.motor.setting.MotorSetting;
+import frc.bofalib.generic.hardware.motor.talonfx.control.TalonFXControl;
+import frc.bofalib.generic.hardware.motor.talonfx.control.TalonFXControlEmpty;
+import frc.bofalib.generic.hardware.motor.talonfx.query.TalonFXQuery;
+import frc.bofalib.generic.loggable.MockLoggableBase;
+
+final class TalonFXWrapperMock extends 
+    MockLoggableBase 
+implements 
+    TalonFXWrapper 
+{
+
+    TalonFXWrapperMock(String name) {
+        super(name);
+    }
+
+    @Override
+    public TalonFXControl getSetControl(MotorSetting motorSetting) {
+        // TODO Auto-generated method stub
+        return TalonFXControlEmpty.getInstance();
+    }
+
+    @Override
+    public void beginControl(TalonFXControl control) {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public void runControl() {
+        // TODO Auto-generated method stub
+    }
+
+    @Override
+    public TalonFXCommonConfigurator getConfigurator() {
+        // TODO Auto-generated method stub
+        return new TalonFXMockConfigurator();
+    }
+
+    @Override
+    public DoubleSupplier queryDouble(TalonFXQuery query) {
+        // TODO Auto-generated method stub
+        return () -> 0.0;
+    }
+
+    @Override
+    public void addToOrchestra(Orchestra orchestra) {
+        // TODO Auto-generated method stub
+    }
+        
+}
