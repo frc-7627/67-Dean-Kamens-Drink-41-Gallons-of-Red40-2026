3374e09068d6504a40136340bdb3458a1f3cf04b
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-17 17:41:46 -0800

    Implemented logging in logging controllable

--- src/main/java/frc/bofalib/loggable/Loggable.java
+++ src/main/java/frc/bofalib/loggable/Loggable.java
@@ -1,9 +1,15 @@
 package frc.bofalib.loggable;
 
+import java.util.logging.Logger;
+
 public interface Loggable {
     String getLoggableName();
 
     default String getLoggableInfo() {
         return getLoggableName();
+    }
+
+    default Logger getLogger() {
+        return Logger.getLogger(getClass().getName());
     }
 }
