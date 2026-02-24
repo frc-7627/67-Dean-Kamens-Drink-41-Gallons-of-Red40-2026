90569941cbed37759adfe4d5d40f3436b442be58
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-24 15:25:33 -0800

    Added utility methods

--- src/main/java/frc/robot/subsystems/controllable/launcher/Launcher.java
+++ src/main/java/frc/robot/subsystems/controllable/launcher/Launcher.java
@@ -1,9 +1,27 @@
 package frc.robot.subsystems.controllable.launcher;
 
+import static edu.wpi.first.units.Units.RadiansPerSecond;
+import static edu.wpi.first.units.Units.RotationsPerSecond;
+import static frc.robot.Constants.LauncherConstants.FLYWHEEL_RADIUS_FEET;
+
 import frc.bofalib.control.Controllable;
 import frc.bofalib.generic.music.MusicalSubsystem;
 
 public interface Launcher extends MusicalSubsystem, Controllable<LauncherControl> {
+    static double toLinearVelocityFPS(double angularVelocityRPS) {
+        return RadiansPerSecond.convertFrom(
+            angularVelocityRPS, 
+            RotationsPerSecond
+        ) * FLYWHEEL_RADIUS_FEET;
+    }
+
+    static double toAngularVelocityRPS(double linearVelocityFPS) {
+        return RotationsPerSecond.convertFrom(
+            linearVelocityFPS / FLYWHEEL_RADIUS_FEET, 
+            RadiansPerSecond
+        );
+    }
+
     static Launcher create() {
         return new LauncherImpl();
     }
