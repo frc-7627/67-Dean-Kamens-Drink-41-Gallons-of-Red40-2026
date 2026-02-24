2dc92eb43a0e6ef2e71bd9f039788061d8496341
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-24 14:45:55 -0800

    Added progressing command builder

---
+++ src/main/java/frc/bofalib/generic/indication/ProgressingCommandBuilder.java
@@ -0,0 +1,33 @@
+package frc.bofalib.generic.indication;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import edu.wpi.first.wpilibj2.command.Command;
+
+public final class ProgressingCommandBuilder {
+    private final ProgressIndicator indicator;
+    private final List<Command> commands;
+    private final List<String> stepNames;
+
+    public ProgressingCommandBuilder(
+        ProgressIndicator indicator
+    ) {
+        this.indicator = Objects.requireNonNull(indicator);
+        this.commands = new ArrayList<>();
+        this.stepNames = new ArrayList<>();
+    }
+
+    public void addStep(
+        String stepName,
+        Command command
+    ) {
+        this.stepNames.add(stepName);
+        this.commands.add(command);
+    }
+
+    public Command build() {
+        return new ProgressingCommand(indicator, commands, stepNames);
+    }
+}
