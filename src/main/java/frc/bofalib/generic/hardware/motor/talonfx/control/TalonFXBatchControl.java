6ed0938eef6f5fba0191f657a03ef166035baf69
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-26 15:00:37 -0800

    Added todo

--- src/main/java/frc/bofalib/generic/hardware/motor/talonfx/control/TalonFXBatchControl.java
+++ src/main/java/frc/bofalib/generic/hardware/motor/talonfx/control/TalonFXBatchControl.java
@@ -2,6 +2,7 @@ package frc.bofalib.generic.hardware.motor.talonfx.control;
 
 import frc.bofalib.loggable.Loggable;
 
+// TODO: unify these into one batch control class.
 public interface TalonFXBatchControl extends Loggable {
     TalonFXControl getLeaderControl();
 
