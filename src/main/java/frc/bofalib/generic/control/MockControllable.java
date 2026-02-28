5580d8aad042882c7dc81b1ac80610d5f35d9053
Gianmarco Lenzi <tyedee09@gmail.com>
2026-02-28 10:02:00 -0800

    Fixed mock controllable

--- src/main/java/frc/bofalib/generic/control/MockControllable.java
+++ src/main/java/frc/bofalib/generic/control/MockControllable.java
@@ -1,21 +1,20 @@
 package frc.bofalib.generic.control;
 
-import frc.bofalib.control.Controllable;
 import frc.bofalib.loggable.Loggable;
 
-public interface MockControllable<Control extends Loggable> extends Controllable<Control>, Loggable {
+public interface MockControllable<Control extends Loggable> extends LoggingControllable<Control> {
     @Override
-    default void beginControl(Control control) {
-        // TODO Auto-generated method stub
+    default void beginControlInner(Control control) {
+        // Do nothing
     }
 
     @Override
-    default void runControl() {
-        // TODO Auto-generated method stub
+    default void runControlInner(Control control) {
+        // Do nothing
     }
 
     @Override
-    default void endControl() {
-        // TODO Auto-generated method stub
+    default void endControlInner(Control control) {
+        // Do nothing
     }
 }
