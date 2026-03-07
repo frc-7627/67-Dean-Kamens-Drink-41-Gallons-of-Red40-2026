ccb0415d4f7a37f854e6e1a5b3d263558c613959
Gianmarco Lenzi <tyedee09@gmail.com>
2026-03-07 12:07:59 -0800

    Fixed logging wrapper command

--- src/main/java/frc/robot/commands/LoggingWrapperCommand.java
+++ src/main/java/frc/robot/commands/LoggingWrapperCommand.java
@@ -5,10 +5,12 @@ import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.WrapperCommand;
 
 public class LoggingWrapperCommand extends WrapperCommand {
-    private final Logger logger = Logger.getLogger(getClass().getName());
+    private final Logger logger;
 
     public LoggingWrapperCommand(Command command) {
         super(command);
+
+        this.logger = Logger.getLogger(command.getClass().getName());
     }
 
     @Override
