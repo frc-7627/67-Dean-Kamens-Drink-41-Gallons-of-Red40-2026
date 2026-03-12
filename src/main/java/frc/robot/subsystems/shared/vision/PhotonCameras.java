ad74198f8aa6464089f420e329795aff34f787ad
Gianmarco Lenzi <tyedee09@gmail.com>
2026-03-12 15:15:13 -0700

    Added commands to restart vision stuff

--- src/main/java/frc/robot/subsystems/shared/vision/PhotonCameras.java
+++ src/main/java/frc/robot/subsystems/shared/vision/PhotonCameras.java
@@ -1,12 +1,22 @@
 package frc.robot.subsystems.shared.vision;
 
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.List;
 import java.util.Optional;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import java.util.stream.Stream;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.Commands;
 import frc.bofalib.dashboard.KeyBuilder;
 import frc.bofalib.subsystem.SharedSubsystemBase;
 
 final class PhotonCameras extends SharedSubsystemBase implements Vision {
+    private static final Logger LOGGER = Logger.getLogger(PhotonCameras.class.getName());
     private static final KeyBuilder KEY_BUILDER = KeyBuilder.of("Vision");
 
     private final List<PhotonCameraWrapper> photonCameraWrappers;
@@ -17,6 +27,16 @@ final class PhotonCameras extends SharedSubsystemBase implements Vision {
     PhotonCameras() {
         this.photonCameraWrappers = List.of(PhotonCameraInfo.values()).stream()
                 .map(photonCameraInfo -> new PhotonCameraWrapper(photonCameraInfo)).toList();
+
+        SmartDashboard.putData(
+            KEY_BUILDER.copyExtendedToString("Restart Vision"), 
+            Commands.runOnce(this::restart)
+        );
+
+        SmartDashboard.putData(
+            KEY_BUILDER.copyExtendedToString("Restart Vision (devices only)"), 
+            Commands.runOnce(this::restartDevicesOnly)
+        );
     }
 
     /**
@@ -40,5 +60,32 @@ final class PhotonCameras extends SharedSubsystemBase implements Vision {
     public Stream<VisionMeasurement> getVisionMeasurements() {
         return photonCameraWrappers.stream().flatMap(photonCameraWrapper -> photonCameraWrapper
                 .getVisionMeasurement(standardDeviations).stream());
+    }
+
+    private void restart() {
+        sendPhotonVisionRequest("10.76.27.15", "restartProgram");
+    }
+
+    private void restartDevicesOnly() {
+        sendPhotonVisionRequest("10.76.27.10", "restartDevice");
+        sendPhotonVisionRequest("10.76.27.11", "restartDevice");
+    }
+
+    private void sendPhotonVisionRequest(String ipString, String command) {
+        try {
+            HttpClient httpClient = HttpClient.newHttpClient();
+            HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI("http://" + ipString + ":5800/api/utils/" + command))
+                .POST(HttpRequest.BodyPublishers.ofString(""))
+                .build()
+            ;
+            httpClient.sendAsync(request, HttpResponse.BodyHandlers.ofString());
+        } catch (Exception error) {
+            LOGGER.log(
+                Level.SEVERE, 
+                error, 
+                () -> "Error while sending photon vision HTTP request to IP: " + ipString
+            );
+        }
     }
 }
