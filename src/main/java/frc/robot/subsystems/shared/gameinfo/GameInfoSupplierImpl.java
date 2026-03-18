9f78bb6eef0f94cf292318ad05f7243b09f2b122
Ryan Wang <rryanwwang@gmail.com>
2026-03-17 17:58:27 -0700

    measure phase times and add to dashboard

--- src/main/java/frc/robot/subsystems/shared/gameinfo/GameInfoSupplierImpl.java
+++ src/main/java/frc/robot/subsystems/shared/gameinfo/GameInfoSupplierImpl.java
@@ -4,6 +4,8 @@ import static frc.robot.Constants.DrivebaseConstants.BLUE_LEFT_FERRY_TARGET_POSI
 import static frc.robot.Constants.DrivebaseConstants.BLUE_RIGHT_FERRY_TARGET_POSITION;
 import static frc.robot.Constants.DrivebaseConstants.RED_LEFT_FERRY_TARGET_POSITION;
 import static frc.robot.Constants.DrivebaseConstants.RED_RIGHT_FERRY_TARGET_POSITION;
+
+import java.sql.Driver;
 import java.util.Optional;
 import java.util.function.Consumer;
 import java.util.logging.Logger;
@@ -11,13 +13,18 @@ import java.util.logging.Logger;
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.Odometry;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
 import edu.wpi.first.wpilibj.event.BooleanEvent;
 import edu.wpi.first.wpilibj.event.EventLoop;
+import frc.bofalib.dashboard.DashboardItems;
+import frc.bofalib.dashboard.KeyBuilder;
+import frc.bofalib.subsystem.CommandSchedulerWrapper;
 import frc.bofalib.subsystem.SharedSubsystemBase;
+import frc.bofalib.util.FunctionalUtil;
 import frc.robot.Constants;
 import frc.robot.subsystems.controllable.drivebase.Side;
 import swervelib.SwerveDrive;
@@ -25,12 +32,16 @@ import swervelib.SwerveDrive;
 final class GameInfoSupplierImpl extends SharedSubsystemBase implements GameInfoSupplier {
     private static final Logger LOGGER = Logger.getLogger(GameInfoSupplier.class.getName());
 
+    private static final String LOGGABLE_NAME = "Game Info";
+    private static final KeyBuilder KEY_BUILDER = KeyBuilder.of(LOGGABLE_NAME);
+
     String gameData = DriverStation.getGameSpecificMessage();
     private final EventLoop eventLoop = new EventLoop();
     private final BooleanEvent allianceSetEvent;
     private Phase phase = Constants.GameInfoConstants.START_PHASE;
     private Alliance alliance;
     private boolean isDistinctAlliance = false;
+    private final Timer teleopTimer = new Timer();
     private final Timer phaseTimer = new Timer();
 
     /**
@@ -45,32 +56,42 @@ final class GameInfoSupplierImpl extends SharedSubsystemBase implements GameInfo
         allianceSetEvent.ifHigh(() -> {
             LOGGER.info("Alliance set from driver station.");
         });
+
+        phaseTimer.start();
+
+        CommandSchedulerWrapper.getInstance().registerPeriodicAction(
+                FunctionalUtil.composeConditional(
+                        DashboardItems.createDoublePusher(
+                                KEY_BUILDER.copyExtendedToString("Time Left in Phase"),
+                                true),
+                        () -> getTimeLeftInPhase(),
+                        FunctionalUtil.hasChangedDoublePredicate()));
     }
 
     @Override
     public Phase getPhase() {
 
-        if (phaseTimer.hasElapsed(110)) {
+        if (teleopTimer.hasElapsed(110)) {
             phase = Phase.ENDGAME;
-        } else if (phaseTimer.hasElapsed(85)) {
+        } else if (teleopTimer.hasElapsed(85)) {
             if (gameData.charAt(0) == 'R') {
                 phase = Phase.TELEOP_ACTIVE;
             } else {
                 phase = Phase.TELEOP_INACTIVE;
             }
-        } else if (phaseTimer.hasElapsed(60)) {
+        } else if (teleopTimer.hasElapsed(60)) {
             if (gameData.charAt(0) == 'B') {
                 phase = Phase.TELEOP_ACTIVE;
             } else {
                 phase = Phase.TELEOP_INACTIVE;
             }
-        } else if (phaseTimer.hasElapsed(35)) {
+        } else if (teleopTimer.hasElapsed(35)) {
             if (gameData.charAt(0) == 'R') {
                 phase = Phase.TELEOP_ACTIVE;
             } else {
                 phase = Phase.TELEOP_INACTIVE;
             }
-        } else if (phaseTimer.hasElapsed(10)) {
+        } else if (teleopTimer.hasElapsed(10)) {
             if (gameData.charAt(0) == 'B') {
                 phase = Phase.TELEOP_ACTIVE;
             } else {
@@ -83,9 +104,45 @@ final class GameInfoSupplierImpl extends SharedSubsystemBase implements GameInfo
         return phase;
     }
 
+    private double getTimeOfPhaseBegin() {
+        if (teleopTimer.hasElapsed(110)) {
+            return 110;
+        } else if (teleopTimer.hasElapsed(85)) {
+            return 85;
+        } else if (teleopTimer.hasElapsed(60)) {
+            return 60;
+        } else if (teleopTimer.hasElapsed(35)) {
+            return 35;
+        } else if (teleopTimer.hasElapsed(10)) {
+            return 10;
+        } else {
+            return 0;
+        }
+    }
+
+    private double getTimeOfPhaseEnd() {
+        if (teleopTimer.hasElapsed(110)) {
+            return 140;
+        } else if (teleopTimer.hasElapsed(85)) {
+            return 110;
+        } else if (teleopTimer.hasElapsed(60)) {
+            return 85;
+        } else if (teleopTimer.hasElapsed(35)) {
+            return 60;
+        } else if (teleopTimer.hasElapsed(10)) {
+            return 35;
+        } else {
+            return 10;
+        }
+    }
+
+    private double getTimeLeftInPhase() {
+        return getTimeOfPhaseEnd() - teleopTimer.get();
+    }
+
     @Override
     public void teleopInit() {
-        phaseTimer.start();
+        teleopTimer.start();
     }
 
     @Override
@@ -126,7 +183,6 @@ final class GameInfoSupplierImpl extends SharedSubsystemBase implements GameInfo
 
     @Override
     public boolean isHubActive() {
-        // TODO Auto-generated method stub
         Phase currentPhase = getPhase();
         return switch (currentPhase) {
             case AUTO, TRANSITION, ENDGAME, TELEOP_ACTIVE -> true;
